d6512d5c4fa14ea979e95f8bea06a6898bc63859
Yelena Badeyan <yelena.badeyan@idt.am>
2024-07-02 16:42:58 +0400

    Commit

---
+++ Homework10-Methods/Task8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FibonacciExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the position of the Fibonacci number you want to calculate: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int fibonacciNumber = Fibonacci(n);
+
+            Console.WriteLine($"The {n}th Fibonacci number is: {fibonacciNumber}");
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+    }
+}
---
+++ Homework10-Methods/Task9/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace AnagramChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first string: ");
+            string str1 = Console.ReadLine();
+
+            Console.Write("Enter the second string: ");
+            string str2 = Console.ReadLine();
+
+            bool areAnagrams = AreAnagrams(str1, str2);
+
+            if (areAnagrams)
+            {
+                Console.WriteLine("The strings are anagrams.");
+            }
+            else
+            {
+                Console.WriteLine("The strings are not anagrams.");
+            }
+        }
+
+        static bool AreAnagrams(string str1, string str2)
+        {
+            str1 = str1.Replace(" ", "").ToLower();
+            str2 = str2.Replace(" ", "").ToLower();
+
+            if (str1.Length != str2.Length)
+            {
+                return false;
+            }
+
+            char[] charArray1 = str1.ToCharArray();
+            char[] charArray2 = str2.ToCharArray();
+
+            Array.Sort(charArray1);
+            Array.Sort(charArray2);
+
+            for (int i = 0; i < charArray1.Length; i++)
+            {
+                if (charArray1[i] != charArray2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
